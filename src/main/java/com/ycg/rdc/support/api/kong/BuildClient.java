74656088a6bddeefbea53345606c6504f8fc206d
Kevin SHA <knowasdf@163.com>
2018-06-11 12:10:50 +0800

    UriTool.java中encode部分调整

--- src/main/java/com/ycg/rdc/support/api/kong/BuildClient.java
+++ src/main/java/com/ycg/rdc/support/api/kong/BuildClient.java
@@ -7,17 +7,16 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 public class BuildClient {
 	private static KongAdminDefaultClient client;
 	private static CloseableHttpClient httpClient;
-	static final int MAX_TOTAL_CONNS = 1000;
-	static final int DEFAULT_MAX_PER_ROUTE = 100;
-	private static Object lockObj=new Object();
+	static final int DEFAULT_MAX_TOTAL_CONNS = 1000;
+	static final int DEFAULT_MAX_PER_ROUTE = 200;
 
 	public static KongAdminDefaultClient Build(String server, String charset, int tomeout) {
 		if (client == null) {
-			synchronized (lockObj) {
+			synchronized (KongAdminDefaultClient.class) {
 				if (client == null) {
 					client = new KongAdminDefaultClient(server, charset, tomeout);
 					PoolingHttpClientConnectionManager hcm = new PoolingHttpClientConnectionManager();
-					hcm.setMaxTotal(MAX_TOTAL_CONNS);
+					hcm.setMaxTotal(DEFAULT_MAX_TOTAL_CONNS);
 					hcm.setDefaultMaxPerRoute(DEFAULT_MAX_PER_ROUTE);
 					httpClient = HttpClients.custom().setConnectionManager(hcm)
 							.build();
--- src/main/java/com/ycg/rdc/support/api/kong/UriTool.java
+++ src/main/java/com/ycg/rdc/support/api/kong/UriTool.java
@@ -5,29 +5,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UriTool {
-	public static String MapToURIParam(Map<String, Object> mapParam){
-		final Wrapper result=new Wrapper();
-		if(mapParam!=null&&!mapParam.isEmpty()){
-			mapParam.forEach((k, v)->{
-				result.inner+="&"+k+"="+v;
+	public static String MapToURIParam(Map<String, Object> mapParam) {
+		final Wrapper result = new Wrapper();
+		if (mapParam != null && !mapParam.isEmpty()) {
+			mapParam.forEach((k, v) -> {
+				result.inner += "&" + k + "=" + v;
 			});
-			result.inner=result.inner.substring(1);
+			result.inner = result.inner.substring(1);
 		}
 		return result.inner;
 	}
-	
-	public static void main(String [] args) {
-		HashMap<String, Object> param= new HashMap<>();
+
+	public static void main(String[] args) {
+		HashMap<String, Object> param = new HashMap<>();
 		param.put("name", "张三");
 		param.put("id", "456");
 		param.put("ctime", new Date());
 		param.put("index", 123L);
 		System.out.println(MapToURIParam(param));
-		System.out.println(MapToURIParam(param));
-		System.out.println(MapToURIParam(param));
 	}
-	
+
 	public static class Wrapper {
-	    public String inner="";
+		public String inner = "";
 	}
 }
